061795b73186e0a28ae9719edf446beaab3132b8
Eric Batista <batistaeric62@gmail.com>
2024-06-07 21:28:06 -0300

    add query and pagination tasks

--- src/pages/Tasks/index.tsx
+++ src/pages/Tasks/index.tsx
@@ -1,9 +1,65 @@
+import { Pagination } from "../../components/Pagination";
+import { TaskCard } from "../../components/TaskCard";
+import { useQueryTasks } from "../../hooks/useQueryTasks";
 import { Container } from "./styles";
 
 export function Tasks() {
+  const {
+    data,
+    page,
+    changeLimit,
+    changePage,
+    nextPage,
+    prevPage,
+    totalPages,
+    isLoading,
+    error,
+  } = useQueryTasks();
+
+  if (totalPages > 0 && page > totalPages) {
+    changePage(totalPages);
+  }
+
   return (
     <Container>
-      <h1>Tasks</h1>
+      <div className="headPageTasks">
+        <h2>Lista</h2>
+
+        <div className="paginationDesktop">
+          <Pagination
+            page={page}
+            step={5}
+            changeLimit={changeLimit}
+            nextPage={nextPage}
+            prevPage={prevPage}
+            totalPages={totalPages}
+          />
+        </div>
+      </div>
+
+      {isLoading && <span className="loading">Carregando...</span>}
+      {!isLoading && error && <span className="loading">error...</span>}
+
+      <div className="tasksContainer">
+        {data?.length == 0 ? (
+          <p className="loading">Sem tarefas para mostrar</p>
+        ) : (
+          data?.map((task) => {
+            return <TaskCard data={task} key={task.id} onClick={() => {}} />;
+          })
+        )}
+      </div>
+
+      <div className="paginationMobile">
+          <Pagination
+            page={page}
+            step={5}
+            changeLimit={changeLimit}
+            nextPage={nextPage}
+            prevPage={prevPage}
+            totalPages={totalPages}
+          />
+        </div>
     </Container>
   );
 }
